78749fd5a8e28a60e9729213619ce02289ab2de2
Albert Liu <albertymliu@gmail.com>
2022-03-08 01:13:17 -0500

    starting

--- src/graph.rs
+++ src/graph.rs
@@ -4,7 +4,34 @@ use core::cell::*;
 use core::mem::*;
 use core::sync::atomic::{AtomicUsize, Ordering};
 
-pub enum Op {}
+// bruh, idk what the deal is. idk what kind of system to use here. we'll figure
+// it out later ig.
+
+pub enum ControlKind {
+    BranchNeqZero {
+        conditional: u32,
+        id_if_true: u32,
+        id_if_false: u32,
+    },
+    Jump {
+        id: u32,
+    },
+}
+
+pub enum OpKind {
+    // Stores: no output value
+    StackStore { offset: u16, size: u8, id: u32 },
+    Store8 { location_id: u32, value_id: u32 },
+    Store16 { location_id: u32, value_id: u32 },
+    Store32 { location_id: u32, value_id: u32 },
+    Store64 { location_id: u32, value_id: u32 },
+
+    StackLoad { offset: u16, size: u8 },
+    Load8 { location_id: u32 },
+    Load16 { location_id: u32 },
+    Load32 { location_id: u32 },
+    Load64 { location_id: u32 },
+}
 
 pub struct BasicBlock {}
 
